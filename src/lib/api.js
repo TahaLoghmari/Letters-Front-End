a35d0f3b10017478292a0a097ea2f8eadabc934f
Taha Loghmari <medtahalog@gmail.com>
2025-03-23 08:14:27 +0100

    Added production mode

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
   (import.meta.env.PROD
-    ? "https://inventoryapplicationbackend.onrender.com/api"
+    ? "https://letters-backend.onrender.com/api"
     : "http://localhost:3000/api");
